41b0809007b0096565920f3d3c9f9b2b73565066
ayush <ayushdhewale@gmail.com>
2026-03-19 18:44:52 +0530

    Predict model

--- app.py
+++ app.py
@@ -25,6 +25,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    # For local development; Render will use gunicorn entrypoint instead
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=10000)
